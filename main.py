dec617a422bc3915a8f7533dfd15128f21b750a8
R1vss <151943579+R1vss@users.noreply.github.com>
2023-11-24 21:09:32 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,61 +1,139 @@
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.contrib.middlewares import logging as mw_logging
 from aiogram import executor
+import sqlite3
 
-API_TOKEN = 'secret'
+API_TOKEN = '6402952763:AAFYTu9BlA4z-isaPIPEAC34mxsicy30ryc'
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+dp.middleware.setup(mw_logging.LoggingMiddleware())
 
+# Инициализация базы данных SQLite
+conn = sqlite3.connect('profiles.db')
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS profiles (
+        user_id INTEGER PRIMARY KEY,
+        name TEXT,
+        photo TEXT,
+        description TEXT,
+        faculty TEXT,
+        age INTEGER,
+        interests TEXT,
+        course INTEGER
+    )
+''')
+conn.commit()
+
+# Инициализация словаря профилей
 profiles = {}
+
+# Шаги создания профиля
+STEPS = [
+    "name",
+    "photo",
+    "description",
+    "faculty",
+    "age",
+    "interests",
+    "course"
+]
+
+class ProfileCreationStates:
+    START = "start"
+    COMPLETED = "completed"
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     user_id = message.from_user.id
-    if user_id not in profiles:
-        profiles[user_id] = {}
-        await message.answer("Привет! Давайте создадим ваш профиль. Используйте кнопки ниже:")
-        keyboard = InlineKeyboardMarkup()
-        keyboard.add(InlineKeyboardButton("Изменить фото", callback_data='edit_photo'))
-        keyboard.add(InlineKeyboardButton("Изменить описание", callback_data='edit_description'))
-        keyboard.add(InlineKeyboardButton("Выбрать факультет", callback_data='choose_faculty'))
-        keyboard.add(InlineKeyboardButton("Выбрать возраст", callback_data='choose_age'))
-        keyboard.add(InlineKeyboardButton("Выбрать интересы", callback_data='choose_interests'))
-        keyboard.add(InlineKeyboardButton("Выбрать курс", callback_data='choose_course'))
-        await message.answer("Выберите действие:", reply_markup=keyboard)
-    else:
-        await message.answer("Ваш профиль уже создан. Используйте кнопки ниже:")
-        keyboard = InlineKeyboardMarkup()
-        keyboard.add(InlineKeyboardButton("Просмотреть свой профиль", callback_data='view_profile'))
-        keyboard.add(InlineKeyboardButton("Изменить фото", callback_data='edit_photo'))
-        keyboard.add(InlineKeyboardButton("Изменить описание", callback_data='edit_description'))
-        keyboard.add(InlineKeyboardButton("Выбрать факультет", callback_data='choose_faculty'))
-        keyboard.add(InlineKeyboardButton("Выбрать возраст", callback_data='choose_age'))
-        keyboard.add(InlineKeyboardButton("Выбрать интересы", callback_data='choose_interests'))
-        keyboard.add(InlineKeyboardButton("Выбрать курс", callback_data='choose_course'))
-        await message.answer("Выберите действие:", reply_markup=keyboard)
+    profiles[user_id] = {}
+    profiles[user_id]['state'] = ProfileCreationStates.START
+    await message.answer("Привет! Давайте создадим ваш профиль. Напишите ваше имя:")
 
-# Добавим обработчики для каждой кнопки
-@dp.callback_query_handler(lambda c: c.data.startswith('edit'))
-async def edit_callback_handler(callback_query: types.CallbackQuery):
+@dp.message_handler(lambda message: profiles.get(message.from_user.id, {}).get('state') == ProfileCreationStates.START)
+async def process_name(message: types.Message):
+    user_id = message.from_user.id
+    conn = sqlite3.connect('profiles.db')
+    cursor = conn.cursor()
+    cursor.execute('UPDATE profiles SET name = ? WHERE user_id = ?', (message.text, user_id))
+    conn.commit()
+
+    profiles[user_id]['state'] = STEPS[1]
+    await bot.send_message(user_id, "Теперь отправьте свою фотографию (это может быть фото профиля).")
+
+@dp.message_handler(lambda message: profiles.get(message.from_user.id, {}).get('state') == STEPS[1])
+async def process_photo(message: types.Message):
+    user_id = message.from_user.id
+    conn = sqlite3.connect('profiles.db')
+    cursor = conn.cursor()
+    cursor.execute('UPDATE profiles SET photo = ? WHERE user_id = ?', (message.text, user_id))
+    conn.commit()
+
+    profiles[user_id]['state'] = STEPS[2]
+    await bot.send_message(user_id, "Отправьте описание о себе (короткое представление).")
+
+@dp.message_handler(lambda message: profiles.get(message.from_user.id, {}).get('state') == STEPS[2])
+async def process_description(message: types.Message):
+    user_id = message.from_user.id
+    conn = sqlite3.connect('profiles.db')
+    cursor = conn.cursor()
+    cursor.execute('UPDATE profiles SET description = ? WHERE user_id = ?', (message.text, user_id))
+    conn.commit()
+
+    profiles[user_id]['state'] = STEPS[3]
+    await bot.send_message(user_id, "Выберите ваш факультет:")
+
+@dp.callback_query_handler(lambda c: c.data.startswith('choose_faculty'))
+async def choose_faculty_callback_handler(callback_query: types.CallbackQuery):
     user_id = callback_query.from_user.id
-    edit_type = callback_query.data.split('_')[1]
+    chosen_faculty = callback_query.data.split('_')[2]
 
-    if edit_type == 'photo':
-        await bot.send_message(user_id, "Здесь будет функционал для изменения фото.")
-    elif edit_type == 'description':
-        await bot.send_message(user_id, "Здесь будет функционал для изменения описания.")
-    elif edit_type == 'faculty':
-        await bot.send_message(user_id, "Здесь будет функционал для выбора факультета.")
-    elif edit_type == 'age':
-        await bot.send_message(user_id, "Здесь будет функционал для выбора возраста.")
-    elif edit_type == 'interests':
-        await bot.send_message(user_id, "Здесь будет функционал для выбора интересов.")
-    elif edit_type == 'course':
-        await bot.send_message(user_id, "Здесь будет функционал для выбора курса.")
+    conn = sqlite3.connect('profiles.db')
+    cursor = conn.cursor()
+    cursor.execute('UPDATE profiles SET faculty = ? WHERE user_id = ?', (chosen_faculty, user_id))
+    conn.commit()
+
+    profiles[user_id]['state'] = STEPS[4]
+    await bot.send_message(user_id, f"Факультет успешно изменен на: {chosen_faculty}")
+    await bot.send_message(user_id, "Укажите ваш возраст:")
+
+@dp.message_handler(lambda message: profiles.get(message.from_user.id, {}).get('state') == STEPS[4])
+async def process_age(message: types.Message):
+    user_id = message.from_user.id
+    conn = sqlite3.connect('profiles.db')
+    cursor = conn.cursor()
+    cursor.execute('UPDATE profiles SET age = ? WHERE user_id = ?', (int(message.text), user_id))
+    conn.commit()
+
+    profiles[user_id]['state'] = STEPS[5]
+    await bot.send_message(user_id, "Расскажите о своих интересах:")
+
+@dp.message_handler(lambda message: profiles.get(message.from_user.id, {}).get('state') == STEPS[5])
+async def process_interests(message: types.Message):
+    user_id = message.from_user.id
+    conn = sqlite3.connect('profiles.db')
+    cursor = conn.cursor()
+    cursor.execute('UPDATE profiles SET interests = ? WHERE user_id = ?', (message.text, user_id))
+    conn.commit()
+
+    profiles[user_id]['state'] = STEPS[6]
+    await bot.send_message(user_id, "Укажите ваш курс:")
+
+@dp.message_handler(lambda message: profiles.get(message.from_user.id, {}).get('state') == STEPS[6])
+async def process_course(message: types.Message):
+    user_id = message.from_user.id
+    conn = sqlite3.connect('profiles.db')
+    cursor = conn.cursor()
+    cursor.execute('UPDATE profiles SET course = ? WHERE user_id = ?', (int(message.text), user_id))
+    conn.commit()
+
+    profiles[user_id]['state'] = ProfileCreationStates.COMPLETED
+    await bot.send_message(user_id, "Профиль успешно создан!")
 
 @dp.callback_query_handler(lambda c: c.data.startswith('view'))
 async def view_callback_handler(callback_query: types.CallbackQuery):
@@ -63,15 +141,31 @@ async def view_callback_handler(callback_query: types.CallbackQuery):
     view_type = callback_query.data.split('_')[1]
 
     if view_type == 'profile':
-        profile = profiles[user_id]
+        profile = profiles.get(user_id, {})
         name = profile.get('name', 'Имя не указано')
         interests = profile.get('interests', 'Интересы не указаны')
         faculty = profile.get('faculty', 'Факультет не указан')
         age = profile.get('age', 'Возраст не указан')
         course = profile.get('course', 'Курс не указан')
         await bot.send_message(user_id, f"Ваш профиль:\nИмя: {name}\nФакультет: {faculty}\nВозраст: {age}\nИнтересы: {interests}\nКурс: {course}")
-    elif view_type == 'all_profiles':
-        await bot.send_message(user_id, "Здесь будет функционал для просмотра анкет в базе данных.")
+    elif view_type == 'profiles':
+        # Здесь будет функционал для просмотра анкет в базе данных
+        pass
+    elif view_type == 'edit':
+        profiles[user_id]['state'] = ProfileCreationStates.START
+        await bot.send_message(user_id, "Изменение профиля. Напишите ваше имя:")
+
+@dp.callback_query_handler(lambda c: c.data.startswith('edit_profile'))
+async def edit_profile_callback_handler(callback_query: types.CallbackQuery):
+    user_id = callback_query.from_user.id
+    profiles[user_id]['state'] = ProfileCreationStates.START
+    await bot.send_message(user_id, "Изменение профиля. Напишите ваше имя:")
+
+@dp.callback_query_handler(lambda c: c.data.startswith('view_profiles'))
+async def view_profiles_callback_handler(callback_query: types.CallbackQuery):
+    user_id = callback_query.from_user.id
+    # Здесь будет функционал для просмотра анкет в базе данных
+    pass
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
